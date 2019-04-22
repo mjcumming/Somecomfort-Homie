dc5c522bcdb5bfac08ad47bc6b397f5621451c0b
mjcumming <mike@4831.com>
2019-04-21 19:59:03 -0500

    setup

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='Somecomfort Homie 3',
-      version='0.0.1',
-      description='Homie 3.0.1 for Honeywell Total Comfort North America',
+      version='0.0.2',
+      description='Homie 3 for Honeywell Total Comfort North America',
       author='Michael Cumming',
       author_email='mike@4831.com',
       long_description=long_description,
